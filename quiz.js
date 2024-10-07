6545aacb64d4cde17fed85fbcb16b886586075dd
khoulidranya <khoulidranya@gmail.com>
2024-10-07 14:51:34 +0200

    Update quiz.js

--- quiz.js
+++ quiz.js
@@ -1,1 +1,17 @@
 
+function checkAnswer() {
+    let correctAnswer = "4";
+    let userAnswer = document.querySelector('input[name="quiz"]:checked');
+    if (userAnswer) {
+        userAnswer = userAnswer.value;
+        if (userAnswer === correctAnswer) {
+            document.getElementById("feedback").textContent = "Correct! Well done.";
+        } else {
+            document.getElementById("feedback").textContent = "That's incorrect. Try again!";
+        }
+    } else {
+        document.getElementById("feedback").textContent = "Please select an answer!";
+    }
+}
+document.getElementById("submit-answer").addEventListener("click", checkAnswer);
+
